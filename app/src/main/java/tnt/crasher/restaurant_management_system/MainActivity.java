0ec96705c05b1486e476d0c1b1c2fe199e852972
Sherwyne <sherwyne123+work@gmail.com>
2019-02-20 15:34:28 +0800

    Fix some changes...

--- app/src/main/java/tnt/crasher/restaurant_management_system/MainActivity.java
+++ app/src/main/java/tnt/crasher/restaurant_management_system/MainActivity.java
@@ -11,46 +11,21 @@ import android.widget.RatingBar;
 import tnt.crasher.restaurant_management_system.User.MenuDish;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button_user;
-    private RatingBar ratingBar;
-
+    private Button button_guest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page_layout);
 
-        button_user = findViewById(R.id.button_user);
-        ratingBar = findViewById(R.id.ratingBar2);
+        button_guest = findViewById(R.id.button_guest);
 
-        ratingBar.setFocusable(true);
-
-        button_user.setOnClickListener(new View.OnClickListener() {
+        button_guest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MenuDish.class);
                 startActivity(intent);
             }
         });
-
-        View.OnTouchListener listenerRatingBar = new View.OnTouchListener() {
-            float xDown;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // save down X coordinate
-                    xDown = event.getX();
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    // if user moves do not move the finger, update RatingBar value
-                    if (Math.abs(xDown - event.getX()) < 5) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        };
-
-        ratingBar.setOnTouchListener(listenerRatingBar);
     }
 }
