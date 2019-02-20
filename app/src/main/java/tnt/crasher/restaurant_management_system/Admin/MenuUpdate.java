392521b5f43efb1c22f58870aa78e857f78077dc
KeijiMizumura <keijimizumura09@gmail.com>
2019-02-20 14:20:09 -0800

    admin update.

---
+++ app/src/main/java/tnt/crasher/restaurant_management_system/Admin/MenuUpdate.java
@@ -0,0 +1,15 @@
+package tnt.crasher.restaurant_management_system.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tnt.crasher.restaurant_management_system.R;
+
+public class MenuUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_menu);
+    }
+}
